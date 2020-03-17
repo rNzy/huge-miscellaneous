51967d3785ca418a8782c21e48c20e5027d8bd1f
rNzy <philippequemener1@gmail.com>
2020-03-17 16:56:44 +0100

    wip: cs_players-front

--- vue/cs_players-front/src/App.vue
+++ vue/cs_players-front/src/App.vue
@@ -1,28 +1,29 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Nav />
+    <SinglePage version="v0.1.0" />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import SinglePage from "./components/SinglePage.vue";
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    HelloWorld
+    SinglePage
   }
-}
+};
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="sass">
+@import "bulma"
+
+#app
+  font-family: Avenir, Helvetica, Arial, sans-serif
+  -webkit-font-smoothing: antialiased
+  -moz-osx-font-smoothing: grayscale
+  text-align: center
+  color: #2c3e50
 </style>
+
---
+++ vue/cs_players-front/src/components/SinglePage.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="container">
+    <h1 class="is-size-1">Welcome to CS Players ! {{ version }}</h1>
+    <h2 class="is-size-2">Players</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SinglePage",
+  props: {
+    version: String
+  }
+};
+</script>
+
+<style lang="sass"></style>
